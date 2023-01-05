d67ba17e5828de7db583b8afc8a4dce4fdf506b6
ashutosh3027 <iit2020072@iiita.ac.in>
2023-01-05 23:53:25 +0530

    added author name

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Component/Home';
 function App() {
   return (
     <div className="App">
+       <div class="Author">&#60;Ashutosh Verma/&#62;</div>
       <Home/>
     </div>
   );
